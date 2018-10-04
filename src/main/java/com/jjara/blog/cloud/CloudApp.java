8d8c6dc7cca7fd35c8fa592eff8ed0dc96ffcce9
JJaraM <jonathan.jara.morales@gmail.com>
2018-10-03 22:18:00 -0600

    Fix issues with Eureka integration

--- src/main/java/com/jjara/blog/cloud/CloudApp.java
+++ src/main/java/com/jjara/blog/cloud/CloudApp.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class App {
+public class CloudApp {
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(CloudApp.class, args);
 	}
 }
 
