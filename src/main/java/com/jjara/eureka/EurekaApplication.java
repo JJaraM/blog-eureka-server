757ab788fbc182cf1ddd3f21ccdf9ad8cfcfd3fb
JJaraM <jonathan.jara.morales@gmail.com>
2021-05-02 12:17:50 +0100

    Add services

--- src/main/java/com/jjara/eureka/EurekaApplication.java
+++ src/main/java/com/jjara/eureka/EurekaApplication.java
@@ -1,10 +1,6 @@
 package com.jjara.eureka;
 
-import com.netflix.discovery.DiscoveryClient;
-import com.netflix.discovery.shared.Application;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
-import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
--- src/main/java/com/jjara/eureka/ServicesEndPoint.java
+++ src/main/java/com/jjara/eureka/ServicesEndPoint.java
@@ -1,10 +1,13 @@
 package com.jjara.eureka;
 
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import com.netflix.discovery.shared.Application;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -18,7 +21,14 @@ public class ServicesEndPoint {
     }
 
     @ReadOperation
-    public List<String> services() {
-        return discoveryClient.getServices();
+    public List<ServiceInstance> services() {
+        List<String> services = this.discoveryClient.getServices();
+        List<ServiceInstance> instances = new ArrayList<>();
+        services.forEach(serviceName -> {
+            this.discoveryClient.getInstances(serviceName).forEach(instance ->{
+                instances.add(instance);
+            });
+        });
+        return instances;
     }
 }
