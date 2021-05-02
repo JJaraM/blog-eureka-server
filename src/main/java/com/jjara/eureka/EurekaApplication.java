c9fe8b804a68aed7dac1a571a59ade72c5f72132
JJaraM <jonathan.jara.morales@gmail.com>
2021-05-02 12:13:29 +0100

    Add actuator end point

--- src/main/java/com/jjara/eureka/EurekaApplication.java
+++ src/main/java/com/jjara/eureka/EurekaApplication.java
@@ -1,11 +1,17 @@
 package com.jjara.eureka;
 
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.shared.Application;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @EnableEurekaServer
-@SpringBootApplication
+@EnableAutoConfiguration
 public class EurekaApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/jjara/eureka/ServicesEndPoint.java
@@ -0,0 +1,24 @@
+package com.jjara.eureka;
+
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import com.netflix.discovery.shared.Application;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+import java.util.List;
+
+@Component
+@Endpoint(id="services")
+public class ServicesEndPoint {
+
+    private final DiscoveryClient discoveryClient;
+
+    public ServicesEndPoint(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    @ReadOperation
+    public List<String> services() {
+        return discoveryClient.getServices();
+    }
+}
