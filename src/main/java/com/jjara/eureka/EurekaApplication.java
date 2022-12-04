80de99eab42a075bedc3067a264bedd9f068fa6d
Jonathan <jonathanjm100@gmail.com>
2022-12-04 15:27:06 +0000

    Test issues withs startup

--- src/main/java/com/jjara/eureka/EurekaApplication.java
+++ src/main/java/com/jjara/eureka/EurekaApplication.java
@@ -2,12 +2,12 @@ package com.jjara.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
 @EnableEurekaServer
-@EnableAutoConfiguration
+@SpringBootApplication
 public class EurekaApplication {
 
 	public static void main(String[] args) {
